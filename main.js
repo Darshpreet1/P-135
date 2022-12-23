09f47d494ba3a52bcb7d9cf08af3666ccf4cea1f
Darshpreet1 <102862814+Darshpreet1@users.noreply.github.com>
2022-12-23 19:50:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 
 function setup(){
     canvas = createCanvas(480,500);
+    console.log(canvas.width)
     video =  createCapture(VIDEO);
     canvas.position(800,100);
     posenet = ml5.poseNet(video , modelloded);
     posenet.on("pose",gotposes);
 }
 
-
+function draw(){
+    background("grey")
+}
 
 function modelloded(){
     console.log("modelloded")
